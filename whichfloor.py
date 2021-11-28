fdc1038c7dceeae04eec1c0fef6fe80d8f66ce5b
MonadoDay <89417555+MonadoDay@users.noreply.github.com>
2021-11-27 23:35:53 -0500

    Add files via upload

---
+++ whichfloor.py
@@ -0,0 +1,9 @@
+var maximum_stories = input("On what floor of the building is your office?")
+var usernum = int(maximum_stories)
+
+while (usernum >= 101:
+  print(str("You selected " + usernum + ". The building has 100 floors, enter a valid number!")
+  maximum_stories = input("On what floor of the building is your office?")
+  usernum = int(maximum_stories)
+
+print(str("Heading to the " + usernum + "th floor")
