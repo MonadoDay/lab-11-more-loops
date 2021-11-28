cdf252033249b0e7fdcae2f2cff608eebc96938e
MonadoDay <89417555+MonadoDay@users.noreply.github.com>
2021-11-27 22:23:00 -0500

    Add files via upload

---
+++ gimmiemynumber.py
@@ -0,0 +1,9 @@
+userstring = input("give me a number greater than 100")
+usernum = int(userstring)
+
+while usernum <= 100:
+  print(str(usernum) + " is less than 100, dummy. Try again, I've got all day...")
+  userstring = input("give me a number greater than 100")
+  usernum = int(userstring)
+
+print(str(usernum)(usernum + " is over 100. Great job!")
